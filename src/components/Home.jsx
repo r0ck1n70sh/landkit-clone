8ce263d415a91fecb4095e488fc81ff80f503fe0
r0ck1n70sh <r0ck1n70sh@gmail.com>
2021-07-10 18:41:05 +0530

    home added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,42 +5,42 @@ import './Home.css';
 
 const Home = () => {
     return (
-        <div className='Home'>
-            <div className='card-upper'>
+        <div className='home'>
+            <div className='card-upper-img'>
                 <img src={ image } alt='illustration-2.png' />
             </div>
-            <div className='card-upper'>
-                <div>
+            <div className='card-upper-text'>
+                <div className='card-upper-text-lg'>
                     Welcome to Landkit.<br />Develop anything.
                 </div>
-                <div>
+                <div className='card-upper-text-sm'>
                     Build a beautiful, modern website with flexible<br /> Bootstrap components built from scratch.
                 </div>
-                <div>
-                    <button>View all Pages</button>
-                    <button>Documentation</button>
+                <div className='btn-container'>
+                    <button className='btn-blue'>View all Pages &rarr;</button>
+                    <button className='btn-grey'>Documentation</button>
                 </div>
             </div>
-            <div className='card-lower'>
-                <div class='card-lower-icon'></div>
-                <div class='card-lower-header'>Built for developers
+            <div className='card-lower card-lower-1'>
+                <div className='card-lower-icon fa fa-toggle-on'></div>
+                <div className='card-lower-header'>Built for developers
 </div>
-                <div class='card-lower-text'>
+                <div className='card-lower-text'>
                     Landkit is built to make your life easier. Variables, build tooling, documentation, and reusable components.
                 </div>
             </div>
-            <div className='card-lower'>
-                <div class='card-lower-icon'></div>
-                <div class='card-lower-header'>Designed to be modern</div>
-                <div class='card-lower-text'>
+            <div className='card-lower card-lower-2'>
+                <div className='card-lower-icon fa fa-list-alt'></div>
+                <div className='card-lower-header'>Designed to be modern</div>
+                <div className='card-lower-text'>
                     Designed with the latest design trends in mind. Landkit feels modern, minimal, and beautiful.
                 </div>
             </div>
-            <div className='card-lower'>
-                <div class='card-lower-icon'></div>
-                <div class='card-lower-header'>Documentation for everything
+            <div className='card-lower card-lower-3'>
+                <div className='card-lower-icon fa fa-code'></div>
+                <div className='card-lower-header'>Documentation for everything
 </div>
-                <div class='card-lower-text'>
+                <div className='card-lower-text'>
                     We've written extensive documentation for components and tools, so you never have to reverse engineer anything.
                 </div>
             </div>
